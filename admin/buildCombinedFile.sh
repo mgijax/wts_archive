86b4b8a818f36081f571cb5ca3c2bc59b6bf0fac
jsb <jsb@jax.org>
2021-04-28 11:43:43 -0400

    added default config file and script to produce combined TR file with no HTML tags

---
+++ admin/buildCombinedFile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Build a single file that combines all the archived WTS TR files into a single one, prepends the
+# TR number to each line, and strips out all HTML tags.
+
+. ../Configuration
+
+if [ "${WTS_ARCHIVE_PATH}" == "" ]; then
+        echo "Error: Missing WTS_ARCHIVE_PATH"
+        exit 1
+fi
+
+cd ${WTS_ARCHIVE_PATH}
+
+echo "Building noTags.txt..."
+for file in `ls */TR*html`; do
+        cat ${file} | sed 's/<[^>]*>//g ; /^$/d' | sed 's@^@'"${file}"' @' >> noTags.txt
+done
+
+LINE_COUNT=`wc -l allNoTags.txt | awk '{print $1}'`
+echo "- Built ${WTS_ARCHIVE_PATH}noTags.txt with ${LINE_COUNT} lines"
